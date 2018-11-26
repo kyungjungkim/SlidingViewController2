6bb29176095ddc0fdcb9104e1403897180467252
kyungjung kim <kj.kim@live.co.kr>
2018-11-26 16:52:11 +0900

    Update SlidingViewController.swift

--- SlidingViewController.swift
+++ SlidingViewController.swift
@@ -44,7 +44,7 @@ class SlidingViewController: UIViewController, UITableViewDelegate, UITableViewD
     // UITableViewDelegate
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return heightOfCell;
+        return heightOfCell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -69,7 +69,7 @@ class SlidingViewController: UIViewController, UITableViewDelegate, UITableViewD
     // UITableViewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayData.count;
+        return arrayData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -91,33 +91,45 @@ class SlidingViewController: UIViewController, UITableViewDelegate, UITableViewD
     // Methods
     
     func openAnimation() -> () {
-
+        self.view.isHidden = false
+        
+        UIView.animate(withDuration: TimeInterval(open), delay: TimeInterval(0), options: [.curveLinear], animations: {
+            UIView.setAnimationRepeatCount(Float(1.0))
+        }, completion: { finished in
+            if (self.type == AnimationType.BOTH || self.type == AnimationType.GROW) {
+                var tempFrame: CGRect = self.uiTableView.frame
+                tempFrame.origin.x = self.uiTableView.frame.origin.x
+                tempFrame.origin.y = self.uiTableView.frame.origin.y
+                tempFrame.size.width = self.uiTableView.frame.size.width
+                tempFrame.size.height = self.heightTableView
+            }
+            
+            if (self.type == AnimationType.BOTH || self.type == AnimationType.BLENDIN) {
+                self.view.alpha = 1;
+            }
+        })
     }
     
     func closeAnimation() -> () {
-        UIView.animate(withDuration: 0.7, delay: 0.0, options: [.curveLinear], animations: {
-            UIView.setAnimationDuration(TimeInterval(self.close))
+        UIView.animate(withDuration: TimeInterval(close), delay: TimeInterval(0), options: [.curveLinear], animations: {
             UIView.setAnimationRepeatCount(Float(1.0))
-            UIView.setAnimationDelay(TimeInterval(0))
         }, completion: { finished in
+            if (self.type == AnimationType.BOTH || self.type == AnimationType.GROW) {
+                var tempFrame: CGRect = self.uiTableView.frame
+                tempFrame.origin.x = self.uiTableView.frame.origin.x
+                tempFrame.origin.y = self.uiTableView.frame.origin.y
+                tempFrame.size.width = self.uiTableView.frame.size.width
+                tempFrame.size.height = self.heightTableView
+                
+                self.uiTableView.frame = tempFrame
+            }
             
+            if (self.type == AnimationType.BOTH || self.type == AnimationType.BLENDIN) {
+                self.view.alpha = 0;
+            }
+            
+            self.perform(#selector(self.hideView), with: nil, afterDelay: TimeInterval(self.close))
         })
-        
-        if (type == AnimationType.BOTH || type == AnimationType.GROW) {
-            var tempFrame: CGRect = self.uiTableView.frame
-            tempFrame.origin.x = uiTableView.frame.origin.x
-            tempFrame.origin.y = uiTableView.frame.origin.y
-            tempFrame.size.width = uiTableView.frame.size.width
-            tempFrame.size.height = heightTableView
-            
-            self.uiTableView.frame = tempFrame
-        }
-        
-        if (type == AnimationType.BOTH || type == AnimationType.BLENDIN) {
-            self.view.alpha = 0;
-        }
-        
-        self.perform(#selector(hideView), with: nil, afterDelay: TimeInterval(close))
     }
     
     @objc func hideView() -> () {
