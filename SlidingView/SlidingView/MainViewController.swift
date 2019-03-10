23e53ec52100a0d20d012bafa72e4adb35855de5
kyungjung kim <kj.kim@live.co.kr>
2019-03-10 09:50:43 +0900

    Add files via upload

---
+++ SlidingView/SlidingView/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  SlidingView
+//
+//  Created by Kyungjung Kim on 10/03/2019.
+//  Copyright © 2019 Kyungjung Kim. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    var menu: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+}
