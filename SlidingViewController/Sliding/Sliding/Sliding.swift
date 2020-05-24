7c512af26881c1ecd18097a6be11943aac3c70e0
kyungjung kim <kj.kim@live.co.kr>
2020-05-25 08:07:25 +0900

    Add files via upload

--- SlidingViewController/Sliding/Sliding/Sliding.swift
+++ SlidingViewController/Sliding/Sliding/Sliding.swift
@@ -7,5 +7,35 @@
 //
 
 class Sliding {
+    /*
+    var isMenuOpen: Bool = false
 
+    @objc func menuOpenBtnClick(_ sender: Any) {
+        if (isMenuOpen == false) {
+            UIView.animate(withDuration: 0.8, animations: {
+                self.menu!.frame = CGRect(x: 0.0, y: 30.0, width: self.view.frame.width - 100.0, height: self.view.frame.height - 30.0)
+            }, completion: { finished in
+                if (finished) {
+                    self.cell!.uiBtnAll.frame = CGRect(x: self.cell!.uiBtnAll.frame.origin.x, y: self.cell!.uiBtnAll.frame.origin.y - 10.0, width: self.cell!.uiBtnAll.frame.width, height: self.cell!.uiBtnAll.frame.height)
+                    
+                    self.isMenuOpen = true
+                }
+            })
+        }
+    }
+    
+    @objc func swipeMenuClose() {
+        if (isMenuOpen == true) {
+            UIView.animate(withDuration: 0.8, animations: {
+                self.menu!.frame = CGRect(x: -(self.view.frame.width - 100.0), y: 30.0, width: self.view.frame.width - 100.0, height: self.view.frame.height - 30.0)
+            }, completion: { finished in
+                if (finished) {
+                    self.cell!.uiBtnAll.frame = CGRect(x: self.cell!.uiBtnAll.frame.origin.x, y: self.cell!.uiBtnAll.frame.origin.y + 10.0, width: self.cell!.uiBtnAll.frame.width, height: self.cell!.uiBtnAll.frame.height)
+                    
+                    self.isMenuOpen = false
+                }
+            })
+        }
+    }
+ */
 }
